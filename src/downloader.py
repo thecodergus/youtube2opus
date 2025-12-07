c8e72add9fd99226478eb68b1bfcfc13c4005e2d
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-12-07 13:05:52 -0300

    Melhor pegando o local de destino das thumbmail

--- src/downloader.py
+++ src/downloader.py
@@ -1,5 +1,5 @@
 # src/downloader.py
-from typing import Final
+from typing import Final, Optional
 from pathlib import Path
 import yt_dlp
 from src.types import DownloadResult
@@ -27,11 +27,14 @@ def download_audio(url: str, output_dir: str) -> DownloadResult:
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:  # type: ignore
         info = ydl.extract_info(url, download=True)
-        title = sanitize_filename(info.get("title", "audio").replace('"', ""), platform="windows")  # type: ignore
+        title = sanitize_filename(info.get("title", "audio").replace('"', ""), platform="Universal")  # type: ignore
         thumbnail_url = info.get("thumbnail", "")  # type: ignore
-        audio_path = Path(ydl.prepare_filename(info).replace("\\", "/").replace("webm", "wav"))  # type: ignore
+        audio_path: Optional[str] = None
+        for i in info.get("thumbnails"):  # type: ignore
+            if "filepath" in i:
+                audio_path = i.get("filepath")
     return DownloadResult(
-        audio_path=str(audio_path),
+        audio_path=str(audio_path if audio_path else ""),
         title=title,  # type: ignore
         thumbnail_url=thumbnail_url.replace("\\", "/"),  # type: ignore
     )
