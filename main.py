448c194d86746fc98142cee793c22ad3b8757bfa
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-12-07 13:27:04 -0300

    Revert "Melhor pegando o local de destino das thumbmail"
    This reverts commit c8e72add9fd99226478eb68b1bfcfc13c4005e2d.

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from src.utils import clean_youtube_url
 if __name__ == "__main__":
     # Exemplo de uso: forneça sua lista de links aqui
     youtube_links = [
-        "https://www.youtube.com/watch?v=gA4GxTQtrKw",
-        "https://www.youtube.com/watch?v=ETEg-SB01QY",
-        "https://www.youtube.com/watch?v=w1nGMgaukcE",
-        "https://www.youtube.com/watch?v=osPq9Yb8xm8",
-        "https://www.youtube.com/watch?v=pcFK4HzAlsU",
-        "https://www.youtube.com/watch?v=zMCVp6INpnw",
-        "https://www.youtube.com/watch?v=3rkJ3L5Ce80",
-        "https://www.youtube.com/watch?v=Ymwj0zcqnDw",
-        "https://www.youtube.com/watch?v=C4gVlt46voo",
-        "https://www.youtube.com/watch?v=k7xtF0axbjg",
-        "https://www.youtube.com/watch?v=8pGbGjSD9kA",
-        "https://www.youtube.com/watch?v=xLnhtTlwjak",
-        "https://www.youtube.com/watch?v=L3vjm1gybU4",
-        "https://www.youtube.com/watch?v=ysA7IeyfO3c",
-        "https://www.youtube.com/watch?v=RRDQCuHYONg",
-        "https://www.youtube.com/watch?v=mvA48_mEUMA",
+        "https://www.youtube.com/watch?v=OxYLMplSGwo",
+        "https://www.youtube.com/watch?v=mpZrpOY7ABo",
+        "https://www.youtube.com/watch?v=YxUoWqfedKc",
+        "https://www.youtube.com/watch?v=vT5tC-qZ3lI",
+        "https://www.youtube.com/watch?v=s3vdNNBX98w",
+        "https://www.youtube.com/watch?v=fLko2bFrwLA",
+        "https://www.youtube.com/watch?v=rngLO3tF2mA",
+        "https://www.youtube.com/watch?v=oT0cJl3SnMo",
+        "https://www.youtube.com/watch?v=KtHULOEGUpY",
+        "https://www.youtube.com/watch?v=wmR3REi27ZQ",
+        "https://www.youtube.com/watch?v=NvRUpkTrTGk",
+        "https://www.youtube.com/watch?v=E7sP6t1QyrI",
+        "https://www.youtube.com/watch?v=bpOSxM0rNPM",
+        "https://www.youtube.com/watch?v=V0r0CLhm23M",
+        "https://www.youtube.com/watch?v=SlCXgj2bS_A",
     ]
     youtube_links = list(map(clean_youtube_url, youtube_links))
     process_youtube_links(youtube_links, output_dir="./output")
