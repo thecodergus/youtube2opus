26422e7825303baaf7eb31065602979ef274d979
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-12-10 18:08:16 -0300

    Melhorar para mim

--- src/process_pipeline.py
+++ src/process_pipeline.py
@@ -80,5 +80,5 @@ def process_youtube_links(links: Sequence[str], output_dir: str) -> None:
     ensure_directory_exists(output_dir)
     process_partial = partial(process_link, output_dir=output_dir)
 
-    with Pool(processes=mp.cpu_count()) as pool:
+    with Pool(processes=3) as pool:
         pool.map(process_partial, links)
