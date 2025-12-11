a7c3ebdabadd477b9ba41a6d20d975cbba0a2dbc
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2025-12-11 01:01:07 -0300

    Fazendo dele linear

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     youtube_links = [
         "https://www.youtube.com/watch?v=m8QQR-wQA0I",
         "https://www.youtube.com/watch?v=b4PWTnXKVhE",
-        # "https://www.youtube.com/watch?v=F2kTxddHeZs&list=RDEMqpK9LGA9e6RUqfcXCJBXeQ&index=2",
-        # "https://www.youtube.com/watch?v=voGfr0LeCYk&list=RDEMqpK9LGA9e6RUqfcXCJBXeQ&index=6",
-        # "https://www.youtube.com/watch?v=BjJlBiHYUak&list=RDEMqpK9LGA9e6RUqfcXCJBXeQ&index=7",
-        # "https://www.youtube.com/watch?v=Hjs0mArKmrE&list=RDEMqpK9LGA9e6RUqfcXCJBXeQ&index=9",
+        "https://www.youtube.com/watch?v=F2kTxddHeZs",
+        "https://www.youtube.com/watch?v=voGfr0LeCYk",
+        "https://www.youtube.com/watch?v=BjJlBiHYUak",
+        "https://www.youtube.com/watch?v=Hjs0mArKmrE",
     ]
     youtube_links = list(map(clean_youtube_url, youtube_links))
     process_youtube_links(youtube_links, output_dir="./output")
